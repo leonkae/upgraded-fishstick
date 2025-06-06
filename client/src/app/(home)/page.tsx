9c593db4eb606067b7cd523347afe6310e98607b
CGAJAY <jmugendi737@gmail.com>
2025-06-06 09:53:29 +0300

    Removed header and footer from 'app/'
    - This is because I have the both header and footer components in home layout.tsx

--- client/src/app/(home)/page.tsx
+++ client/src/app/(home)/page.tsx
@@ -1,23 +1,19 @@
-import { Header } from "../../components/shared/header";
 import { DiscoverYourPath } from "../../components/home/discoverYourPath";
 import { Hero } from "../../components/home/hero";
 import { HowItWorks } from "../../components/home/howItWorks";
 import { ReadyToDiscover } from "../../components/home/readyToDiscover";
 import { Testimonials } from "../../components/home/testimonials";
 import { QuizPreview } from "../../components/home/quizPreview";
-import { Footer } from "../../components/shared/footer";
 
 const Home = () => {
   return (
     <>
-      <Header />
       <Hero />
       <HowItWorks />
       <DiscoverYourPath />
       <QuizPreview />
       <Testimonials />
       <ReadyToDiscover />
-      <Footer />
     </>
   );
 };
