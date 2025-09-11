6a57e95f28de1340574fe54acdd4fb45076653b1
Mark Muthii <mark.muthii@strathmore.edu>
2025-09-11 12:46:56 +0300

    Admin: Analytics

--- client/src/app/(protected)/admin/analytics/page.tsx
+++ client/src/app/(protected)/admin/analytics/page.tsx
@@ -1,5 +1,7 @@
+import { Analytics } from "@/components/admin/Analytics";
+
 const AnalyticsPage = () => {
-  return <div>AnalyticsPage</div>;
+  return <Analytics />;
 };
 
 export default AnalyticsPage;
