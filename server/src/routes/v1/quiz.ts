2c997bc5055cfa7c1016907c6ff339257820e9df
leonkae <leon29kae@gmail.com>
2025-09-22 00:03:15 +0300

    create more routes for methods put and delete

--- server/src/routes/v1/quiz.ts
+++ server/src/routes/v1/quiz.ts
@@ -1,9 +1,18 @@
-import { createQuiz, getQuiz } from "@/controllers";
+// import { createQuiz, getQuiz } from "@/controllers";
+import { createQuiz, getQuiz, updateQuiz, deleteQuiz } from "@/controllers";
 import { Router } from "express";
 
 const quizRouter = Router();
 
-quizRouter.post("/", createQuiz);
-quizRouter.get("/", getQuiz);
+function asyncHandler(fn: any) {
+  return function (req: any, res: any, next: any) {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
+}
+
+quizRouter.post("/", asyncHandler(createQuiz));
+quizRouter.get("/", asyncHandler(getQuiz));
+quizRouter.put("/:id", asyncHandler(updateQuiz));
+quizRouter.delete("/:id", asyncHandler(deleteQuiz));
 
 export { quizRouter };
