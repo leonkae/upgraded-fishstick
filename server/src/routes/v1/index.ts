41e70a4863ef1f81e0d3890cb7f0a6d19fbd207a
leonkae <leon29kae@gmail.com>
2025-09-26 18:56:02 +0300

    response routes created

--- server/src/routes/v1/index.ts
+++ server/src/routes/v1/index.ts
@@ -4,6 +4,7 @@ import { authRouter } from "@/routes/v1/auth";
 import { userRouter } from "@/routes/v1/users";
 import { analyticsRouter } from "@/routes/v1/analytics";
 import { quizRouter } from "@/routes/v1/quiz";
+import { responseRouter } from "@/routes/v1/response";
 
 const v1Router = Router();
 
@@ -17,5 +18,6 @@ v1Router.use("/auth", authRouter);
 v1Router.use("/user", userRouter);
 v1Router.use("/quiz", quizRouter);
 v1Router.use("/analytics", analyticsRouter);
+v1Router.use("/responses", responseRouter);
 
 export { v1Router };
