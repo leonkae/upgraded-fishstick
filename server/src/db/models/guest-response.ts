e0a363dec175da0311e007f61c4e6ccdea3a8921
leonkae <leon29kae@gmail.com>
2025-09-26 18:57:55 +0300

    guest response model created - no auth required

---
+++ server/src/db/models/guest-response.ts
@@ -0,0 +1,41 @@
+import { model, Schema, Document } from "mongoose";
+
+interface IGuestResponse extends Document {
+  userInfo: {
+    name: string;
+    email: string;
+    phone: string;
+  };
+  responses: {
+    questionId: string;
+    optionId: string;
+    questionText: string;
+    selectedOption: string;
+    score: number;
+  }[];
+}
+
+const guestResponseSchema = new Schema<IGuestResponse>(
+  {
+    userInfo: {
+      name: { type: String, required: true },
+      email: { type: String, required: true },
+      phone: { type: String, required: true },
+    },
+    responses: [
+      {
+        questionId: { type: String, required: true },
+        optionId: { type: String, required: true },
+        questionText: { type: String, required: true },
+        selectedOption: { type: String, required: true },
+        score: { type: Number, required: true },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const GuestResponseModel = model<IGuestResponse>(
+  "GuestResponse",
+  guestResponseSchema
+);
