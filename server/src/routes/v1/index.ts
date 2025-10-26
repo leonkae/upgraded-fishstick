89acb94a9d39be4164f392b83b9fc5898fff8cc2
leonkae <leon29kae@gmail.com>
2025-10-26 12:50:58 +0300

    stats route added

--- server/src/routes/v1/index.ts
+++ server/src/routes/v1/index.ts
@@ -5,6 +5,7 @@ import { userRouter } from "@/routes/v1/users";
 import { analyticsRouter } from "@/routes/v1/analytics";
 import { quizRouter } from "@/routes/v1/quiz";
 import { responseRouter } from "@/routes/v1/response";
+import { statsRouter } from "@/routes/v1/stats";
 
 const v1Router = Router();
 
@@ -19,5 +20,6 @@ v1Router.use("/user", userRouter);
 v1Router.use("/quiz", quizRouter);
 v1Router.use("/analytics", analyticsRouter);
 v1Router.use("/responses", responseRouter);
+v1Router.use("/stats", statsRouter);
 
 export { v1Router };
